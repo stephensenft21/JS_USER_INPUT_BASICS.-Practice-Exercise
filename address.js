ce161ff08bf1e8af82df2788152e64c330355fb4
Stephen Senft <stephensenft21@gmail.com>
2019-03-07 09:51:44 -0600

    changes made to jsfile

--- address.js
+++ address.js
@@ -1,23 +1,49 @@
-// Some starter code
-const container = document.querySelector("#thingLocation")
+let addressBook = [
+    {
+        name: "Jisie",
+        address: "5918 Brook Bend Dr"
+    },
+    {
+        name: "Kristen",
+        address: "452 Moss Trail"
+    },
+    {
+        name: "Steve",
+        address: "500 Interstate Blvd S"
+    }
+]
+
+const container = document.querySelector("#addressList")
+
+const listAddressOnDom = () => {
+    container.innerHTML = ""
+    debugger
+    addressBook.forEach(element => {
+    container.innerHTML += `
+        <section>
+            <h1>${element.name}</h1>
+            <div>${element.address}</div>
+        </section>
+    `
+    });
+}
+
+listAddressOnDom()
 
 document.querySelector("#saveEntry").addEventListener("click", event => {
     /*
         Collect the user put by selecting the input fields, one
         at a time, and accessing the `value` property
     */
-    const thing = document.querySelector("#thing").value
-    const location = document.querySelector("#location").value
+    const personName = document.querySelector("#fullName").value
+    const personAddress = document.querySelector("#address").value
 
-// console.log("thing",thing.value)
-// console.log("location",location.value)
-    // Once you have collected all the values, update the DOM
-    // with some HTML
-    container.innerHTML += `
-        <section>
-            <h1>I can purchase ${thing} at ${location}.</h1>
-     
-        </section>
-    `
+    let newAddressBookEntry = {
+        name: personName,
+        address: personAddress
+    }
+    // debugger
+    addressBook.push(newAddressBookEntry)
+    listAddressOnDom()
 
 })
